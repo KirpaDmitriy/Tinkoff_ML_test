26e61b6899880addb23d0e0bcc35e412beee17f7
KirpaDmitriy <80625335+KirpaDmitriy@users.noreply.github.com>
2021-09-26 19:22:03 +0300

    Created basic Minesweeper console game platform

---
+++ main.py
@@ -0,0 +1,86 @@
+from random import randint
+field = []  # 'ue' - untouched and empty, 'op' - open, 'fb' - flag with no bomb, 'ub' - untouched with bomb,
+# 'f' - flagged with bomb
+user_field = []
+
+n, m = 5, 5
+bomb_num = 5
+
+oc = 0
+win_num_oc = n * m - bomb_num
+
+
+def print_field(fld):
+    for i in range(len(fld)):
+        print(*fld[i], sep=' ')
+
+
+def bombs_around(fld, x, y):
+    num = 0
+    up_bx = len(fld) - 1
+    up_by = len(fld[0]) - 1
+
+    if x > 0:
+        if fld[x - 1][y] == 'ub':
+            num += 1
+    if y > 0:
+        if fld[x][y - 1] == 'ub':
+            num += 1
+    if x > 0 and y > 0:
+        if fld[x - 1][y - 1] == 'ub':
+            num += 1
+
+    if x < up_bx:
+        if fld[x + 1][y] == 'ub':
+            num += 1
+    if y < up_by:
+        if fld[x][y + 1] == 'ub':
+            num += 1
+    if x < up_bx and y < up_by:
+        if fld[x + 1][y + 1] == 'ub':
+            num += 1
+
+    if x < up_bx and y > 0:
+        if fld[x + 1][y - 1] == 'ub':
+            num += 1
+    if x > 0 and y < up_by:
+        if fld[x - 1][y + 1] == 'ub':
+            num += 1
+
+    return num
+
+
+for i in range(n):
+    field.append(['ue'] * m)
+    user_field.append(['*'] * m)
+for i in range(bomb_num):
+    rand_x, rand_y = randint(0, n - 1), randint(0, m - 1)
+    while field[rand_x][rand_y] != 'ue':
+        rand_x, rand_y = randint(0, n - 1), randint(0, m - 1)
+    field[rand_x][rand_y] = 'ub'
+print(*field, sep='\n')
+for j in range(m * n):
+    x, y, flag = input().split()
+    x, y = int(x) - 1, int(y) - 1
+    state = field[x][y]
+    if flag == 'Flag':
+        if state == 'ub':
+            field[x][y] = 'f'
+        if state == 'ue':
+            field[x][y] = 'fb'
+        user_field[x][y] = 'f'
+    if flag == 'Open':
+        if state == 'ue':
+            field[x][y] = 'op'  # count bombs
+            user_field[x][y] = bombs_around(field, x, y)
+            oc += 1
+            if oc == win_num_oc:
+                print_field(user_field)
+                print('You won!!!!!')
+                break
+        if state == 'ub':
+            print('Oh nooo!!!!!')
+            break
+    print_field(user_field)
+
+print("Hope you enjoyed the game!")
